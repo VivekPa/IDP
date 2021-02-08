2b11d37479f5fd0fbe46ef9d0f757513b5cc8615
Vivek Palaniappan <vivekpalaniappan69@gmail.com>
2021-02-08 18:53:32 +0800

    Added obstacle avoidance function

---
+++ controllers/obstacle_avoidance/obstacles.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+def avoid_obstacle(curr_coord, obs_coord, obs_size):
+    """
+    Provides code to move past blocks that are not of correct colour
+
+    :param curr_coord: current coordinates of the vehicle
+    :type curr_coord: int array
+    :param obs_coord: coordinates of the obstacle
+    :type obs_coord: int array
+    :param obs_size: size of the obstacle
+    :type obs_size: float
+    """
+
+    robot_size = [10, 10] #placeholder for actual size
+
+    dist_to_obj = np.sqrt((curr_coord[0]-obs_coord[0])^2 + (curr_coord[1]-obs_coord[1])^2)
+
+    new1 = [curr_coord[0]+obs_size/2+robot_size[0], curr_coord[1]]
+    new2 = [new1[0], new1[1]+obs_size+2*dist_to_obj+robot_size[1]]
+    new3 = [curr_coord[0], new2[1]]
+
+    return new1, new2, new3
